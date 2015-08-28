70cabd3e103e89e871ef2b724568dffcad38be07
Yuen Shing <y.shing@me.com>
2015-08-28 10:34:44 +0800

    Just Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# loopback-passport
+# loopback-passport-bridge
 Loopback &lt;-> Passport.js Connector
+## Purpose
+A better intergration of Passport.js for loopback.
+### What it does
+
+## Loopback Models
+* AuthProvider model (base: Model)
+* UserPassport model (base: PresistedModel) 
+### AuthProvider Methods
+login: 
+link: 
---
+++ test/model.auth-model.test.js
@@ -0,0 +1,21 @@
+var m = require('./init');
+var loopback = require('loopback');
+var assert = require('assert');
+var AuthProvider = m.AuthProvider;
+var User = loopback.User;
+
+before(function (done) {
+  // User.destroyAll(done);
+  done();
+});
+describe('AuthProvider', function () {
+	it('Should able to add new Providers', function (done) {
+		AuthProvider.use('local','local');
+		assert(Array.isArray(AuthProvider.list()),'return a Array');
+		console.log(AuthProvider.list());
+		done();
+
+		
+	});
+	
+});
